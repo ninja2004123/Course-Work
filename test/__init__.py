34ff8594220b661b786ff3cdcf09cb3026c3a3bd
ninja2004123 <59392541+ninja2004123@users.noreply.github.com>
2021-01-09 17:31:49 +0100

    Create __init__.py

---
+++ test/__init__.py
@@ -0,0 +1,22 @@
+import check50
+import check50.cpp
+
+
+@check50.check()
+def exists():
+    """caesar.c exists."""
+    check50.exists("test.cpp")
+	
+	
+	
+@check50.check(exists)
+def compiles():
+    """caesar.c compiles."""
+    check50.c.compile("test.cpp", lcs50=True)
+	
+	
+	
+@check50.check(compiles)
+def encrypts_a_as_b():
+    """encrypts "a" as "b" using 1 as key"""
+    check50.run("./test").stdout("Hello, Wolrd!").exit(0)
