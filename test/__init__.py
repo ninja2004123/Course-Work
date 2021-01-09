ea151ea86b5a76c17ed6a3bed8bc2455bb405438
ninja2004123 <59392541+ninja2004123@users.noreply.github.com>
2021-01-09 18:42:59 +0100

    Create __init__.py

---
+++ test/__init__.py
@@ -0,0 +1,21 @@
+import check50
+
+
+
+@check50.check():
+    """test.cpp exists"""
+    check50.exists("test.cpp")
+    
+    
+    
+    
+@check50.check(exists)
+def compiles():
+    """test.cpp compiles"""
+    check50.run("make test.cpp")
+    
+    
+@check50.check(compiles)
+def Hello_World():
+    """Prints Hello World!"""
+    check50.run("./test".stdout("Hello World!").exit(0)
