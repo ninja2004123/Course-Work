1621132e4aca5c7f4976b2ca79255d9cd5a4b3e1
ninja2004123 <59392541+ninja2004123@users.noreply.github.com>
2021-01-09 17:39:35 +0100

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -3,19 +3,19 @@ import check50
 
 @check50.check()
 def exists():
-    """caesar.c exists."""
+    """test.cpp exists."""
     check50.exists("test.cpp")
 	
 	
 	
 @check50.check(exists)
 def compiles():
-    """caesar.c compiles."""
+    """test.cpp compiles."""
     check50.c.compile("test.cpp", lcs50=True)
 	
 	
 	
 @check50.check(compiles)
 def encrypts_a_as_b():
-    """encrypts "a" as "b" using 1 as key"""
+    """Runs Hello World"""
     check50.run("./test").stdout("Hello, Wolrd!").exit(0)
